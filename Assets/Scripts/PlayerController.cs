fffbcb0855cc94d0d1cf94b051c6c41be91b86cb
BoBbY-Pal <84893882+BoBbY-Pal@users.noreply.github.com>
2024-08-02 15:36:07 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float laneDistance = 2.5f; // Distance between lanes
+    private int targetLane = 1; // Start in the middle lane
+    private Vector2 swipeDirection;
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame || swipeDirection.x < 0)
+            targetLane = Mathf.Max(targetLane - 1, 0);
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame || swipeDirection.x > 0)
+            targetLane = Mathf.Min(targetLane + 1, 2);
+
+        Vector3 targetPosition = new Vector3((targetLane - 1) * laneDistance, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    public void OnSwipe(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            swipeDirection = context.ReadValue<Vector2>();
+    }
+}
